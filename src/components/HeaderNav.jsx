4d6ec9b0f15f0c6c7fe43e97a4556d00ddac8f6e
Maria P <mariagpirela@gmail.com>
2024-08-17 09:26:11 -0500

    Home link fix

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -5,12 +5,12 @@ export default function HeaderNav() {
   return (
     <header>
         <section className='logo-sec'>
-        <Link to={`/`}><h1 className="logo">My ToDos</h1></Link>
+        <Link to={`/MyToDoList`}><h1 className="logo">My ToDos</h1></Link>
         </section>
         
         <section className='nav-sec'>
           <ul>
-            <li><Link to={`/`} className='btn'>My ToDo List</Link></li>
+            <li><Link to={`/MyToDoList`} className='btn'>My ToDo List</Link></li>
             <li><Link to={`/Contact`} className='btn'>Contact</Link></li>
           </ul>
         </section>
