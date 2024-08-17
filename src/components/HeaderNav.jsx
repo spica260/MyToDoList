36638eca57096347dd47050c6599d1edc313a229
Maria P <mariagpirela@gmail.com>
2024-08-17 09:25:21 -0500

    Added responsive style

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -5,13 +5,13 @@ export default function HeaderNav() {
   return (
     <header>
         <section className='logo-sec'>
-          <h1 className="logo">My ToDos</h1>
+        <Link to={`/`}><h1 className="logo">My ToDos</h1></Link>
         </section>
         
         <section className='nav-sec'>
           <ul>
             <li><Link to={`/`} className='btn'>My ToDo List</Link></li>
-            <li><Link to={`contact`} className='btn'>Contact</Link></li>
+            <li><Link to={`/Contact`} className='btn'>Contact</Link></li>
           </ul>
         </section>
     </header>
